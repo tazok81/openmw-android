304840cc06900ee53f36717970bb93cff29dcbba
Duron27 <duron27@gmail.com>
2023-12-14 17:22:18 -0500

    Update build.sh

--- buildscripts/build.sh
+++ buildscripts/build.sh
@@ -12,7 +12,7 @@ LTO="false"
 BUILD_TYPE="release"
 CFLAGS="-fPIC -w"
 CXXFLAGS="-fPIC -frtti -fexceptions -w"
-LDFLAGS="-Wl,--undefined-version"
+LDFLAGS="-fPIC -Wl,--undefined-version"
 
 usage() {
 	echo "Usage: ./build.sh [--help] [--asan] [--arch arch] [--debug|--release]"
