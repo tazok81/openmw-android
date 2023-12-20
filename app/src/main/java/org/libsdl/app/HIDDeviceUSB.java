4cee5343e6a421ab89834637925d5dd6b8fc7365
Duron27 <duron27@gmail.com>
2023-12-20 05:33:44 -0500

    Add files via upload

--- app/src/main/java/org/libsdl/app/HIDDeviceUSB.java
+++ app/src/main/java/org/libsdl/app/HIDDeviceUSB.java
@@ -57,7 +57,7 @@ class HIDDeviceUSB implements HIDDevice {
                 result = mDevice.getSerialNumber();
             }
             catch (SecurityException exception) {
-                result = "";
+                //Log.w(TAG, "App permissions mean we cannot get serial number for device " + getDeviceName() + " message: " + exception.getMessage());
             }
         }
         if (result == null) {
